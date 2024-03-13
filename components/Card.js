65546f3ddd69c99a5833ddf2720cd245492ba01f
Kevin Carvajal <kevin.carvajal28@hotmail.com>
2024-03-12 21:50:45 -0400

    Debug the code for card.js. Card are up but are not able to like, delete, or preview. It seems like the setevenlisteners.

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,37 @@
+const previewImageModal = document.querySelector('#preview-image');
+const previewImage = previewImageModal.querySelector('.modal__image');
+const previewImageTitle = previewImageModal.querySelector('.modal__image-name')
+const ESC_KEYCODE = 27;
+
+const closeModalWindow = ()  => {
+    previewImageModal.classList.remove('modal_opened');
+    document.removeEventListener('keyup', handleEscUp);
+}
+
+const handleEscUp = (e)  => {
+    e.preventDefault();
+    isEscEvent(e, closeModalWindow);
+}
+
+const isEscEvent = (e, action)  => {
+    const activePreview = document.querySelector('modal_opened');
+    if (e.which === ESC_KEYCODE) {
+        action(activePreview);
+    }
+};
+
+
+
+
 class Card {
     constructor(cardData, cardSelector, handlePreviewImage) {
         this._name = cardData.name;
         this._link = cardData.link;
         this._cardSelector = cardSelector;
         this._handlePreviewImage = handlePreviewImage;
+
+        // this._setEventListeners();
+
     }
 
     
@@ -24,32 +52,32 @@ class Card {
     }
     
     getTemplate() {
-        const cardElement = document.querySelector(this._cardSelector).cloneNode(true);
+        const cardElement = document.querySelector('#card-template').content.cloneNode(true);
         this._handleLikeButton = cardElement.querySelector(".card__like-button");
         this._handleDeleteCard = cardElement.querySelector(".card__delete-button");        
         this._cardImageEl = cardElement.querySelector(".card__image");
         this._cardTitleEl = cardElement.querySelector(".card__title");
         this._cardImageEl.src = this._link;
         this._cardImageEl.alt = this._name;        
-        this._setEventListeners();
         
         return cardElement;
         
     }
     
     _setEventListeners() {
-        this._cardElement.querySelector('.card__like-button').addEventListener('click', () => this._handleLikeButton());
-        this._cardElement.querySelector('.card__delete-button').addEventListener( 'click', () => this._handleDeleteCard);
-        this._cardElement.querySelector('.card__image').addEventListener( 'click', () => this._handlePreviewImage);
+        this._cardElement.querySelector('.card__like-button').addEventListener('click', this._handleLikeButton);
+        this._cardElement.querySelector('.card__delete-button').addEventListener( 'click', this._handleDeleteCard);
+        this._cardElement.querySelector('.card__image').addEventListener( 'click', this._handlePreviewImage);
+
     }
 
-    // getView () {
-        //     this._cardElement = this._getTemplate();
+    getView () {
+    this._cardElement = this.getTemplate();
     
-//     this._cardElement.querySelector('.card__image').style.backgroundImage = `url(${this._link})`;
-//     this._cardElement.querySelector('.card__title').textContent = this._name;
-//     this._setEventListeners();
-//     }
+    this._cardElement.querySelector('.card__image').style.backgroundImage = `url(${this._link})`;
+    this._cardElement.querySelector('.card__title').textContent = this._name;
+    this._setEventListeners();
+    }
 }
 
 export default Card;
