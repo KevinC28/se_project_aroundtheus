e5bc5c092ccdd2ee936cd8c2f6ef0ededbd6f9ca
Kevin Carvajal <kevin.carvajal28@hotmail.com>
2024-05-17 17:28:44 -0400

    Added the address to fetch.

--- src/components/Api.js
+++ src/components/Api.js
@@ -12,10 +12,16 @@ class Api {
   }
 
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers
-    })
-    .then(res => this._getResponseData(res));
+    return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
+     headers: {
+       authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6"
+     }
+   })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    });
   }
 
   getUserInfo() {
