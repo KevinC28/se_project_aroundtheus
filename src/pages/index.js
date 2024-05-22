35e3a79cee4e7d6783519a84e8d24af576a3bf5d
Kevin Carvajal <kevin.carvajal28@hotmail.com>
2024-05-22 07:25:43 -0400

    Updated path to index.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
-import "../pages/index.css";
+import "../pages/index.css"; 
 import Api from "../components/Api.js";
 import Popup from "../components/Popup.js";
 
